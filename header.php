8f40be7d5d87e54b8939087516559cf6d1666e60
AnushArmenienne <hayerdepoitou@hotmail.fr>
2024-10-28 16:47:52 +0100

    header 28/10/24

--- header.php
+++ header.php
@@ -12,42 +12,39 @@
     <div id="wrapper" class="hfeed">
         <header id="header" role="banner">
             <div id="branding">
-                  <img class="logo"src="http://mota-version-finale.local/wp-content/uploads/2024/10/Logo.png" alt="">             
+                <img class="logo" src="http://mota-version-finale.local/wp-content/uploads/2024/10/Logo.png" alt="">
             </div>
-            
+
             <div id="menu">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'header-menu',
-					'container_class' => 'header-menu-class'
-				)
-			);
-			?>
-		</div>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'header-menu',
+                        'container_class' => 'header-menu-class')
+                );
+                ?>
+            </div>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 
-<div class="modal" id="videoModal" tabindex="-1" aria-labelledby="videoModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-xl">
-    <div class="modal-content bg-transparent border-0">
-      <div class="modal-header bg-dark rounded-top-4">
-        <button type="button" class="btn-close btn-close-white shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body overflow-hidden p-0">
-        <div class="bg-black rounded-bottom-4" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen id="videoplayer">
-        <?php echo do_shortcode('[contact-form-7 id="2dca0ca" title="Formulaire de contact 1"]'); ?>
-    </div>
-      </div>
-    </div>
-  </div>
-</div>
+
+            <div class="modal" id="videoModal" tabindex="-1" aria-labelledby="videoModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-xl">
+                    <div class="modal-content bg-transparent border-0">
+                        <div class="modal-header bg-dark rounded-top-4">
+                            <button type="button" class="btn-close btn-close-white shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body overflow-hidden p-0">
+                            <div class="bg-black rounded-bottom-4" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen id="videoplayer">
+                                <?php echo do_shortcode('[contact-form-7 id="2dca0ca" title="Formulaire de contact 1"]'); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </header>
         <div id="container">
             <main id="content" role="main">
-
-            
